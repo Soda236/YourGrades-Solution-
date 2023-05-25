4c4604afd9e20b06ead15b62eb2962b10526dd03
Nikita Kravtsov <nikita.kravtsov236@gmail.com>
2023-05-25 23:59:03 +0300

    Add project files.

---
+++ Digits.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace YourGradeScore
+{
+    internal class Digits
+    {
+        string pattern = @"\d+";
+        Regex regex;
+        MatchCollection matches;
+        float result;
+        int amount;
+        int sum;
+        int grade;
+
+        public float ClearGrades(string draft)
+        {
+            sum = 0;
+            regex = new Regex(pattern);
+
+            try
+            {
+                matches = regex.Matches(draft);
+                amount = matches.Count;
+
+
+                foreach (Match match in matches)
+                {
+                    sum += int.Parse(match.Value);
+                }
+
+                result = (float)sum / amount;
+                Console.WriteLine(result);
+            }
+            catch (Exception)
+            
+            {
+                MessageBox.Show("The input value is incorrect", "Excepiton", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return result;
+
+        }
+
+        public string GradeCalculator(float average)
+        {
+            if (average > 4.5) grade = 5;
+            else if (average > 3.5) grade = 4;
+            else if (average > 2.5) grade = 3;
+            else if (average > 1.5) grade = 2;
+            else grade = 1;
+
+            return grade.ToString();
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace YourGradeScore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string result;
+        string clipboardData;
+        Digits num = new Digits();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            input.Foreground = Brushes.Gray;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e) // Calculate Button
+        {
+            float average;
+            string grade;
+
+            if (input.Foreground == Brushes.Gray) return;
+            else
+            {
+                average = num.ClearGrades(input.Text);
+                grade = num.GradeCalculator(average);
+
+                result = average.ToString("F1");
+                gradeWin.Content = grade;
+
+                if (result == "NaN")
+                {
+                    resultWin.Content = "nil";
+                    gradeWin.Content = "nil";
+                }
+                else resultWin.Content = result;
+            }
+
+            switch (grade)
+            {
+                case "5":
+                    gradeWin.Foreground = Brushes.DeepPink;
+                    break;
+                case "4":
+                    gradeWin.Foreground = Brushes.SpringGreen;
+                    break;
+                case "3":
+                    gradeWin.Foreground = Brushes.Yellow;
+                    break;
+                case "2":
+                    gradeWin.Foreground = Brushes.Red;
+                    break;
+                case "1":
+                    gradeWin.Foreground = Brushes.DarkRed;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void input_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (input.Foreground == Brushes.Gray)
+            {
+                input.Text = "";
+                input.Foreground = Brushes.Black;
+            }
+        }
+
+        private void pasteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                clipboardData = Clipboard.GetText();
+                input.Foreground = Brushes.Black;
+                input.Text = clipboardData.Replace(" ", string.Empty);
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            input.Foreground = Brushes.Black;
+            input.Text = "";
+        }
+    }
+}
